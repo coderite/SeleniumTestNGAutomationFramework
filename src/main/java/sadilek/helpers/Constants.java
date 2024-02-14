e77006f7625616c22ad69fc2a9a159318972dc70
Lucas <4528272+coderite@users.noreply.github.com>
2024-02-14 15:36:43 +0100

    added a Constants file

---
+++ src/main/java/sadilek/helpers/Constants.java
@@ -0,0 +1,12 @@
+package sadilek.helpers;
+
+public class Constants {
+    public static final class TestData {
+        public static final String HIGHLIGHT = "highlight";
+        public static final String PRODUKTART = "produktart";
+        public static final String MARKE = "marke";
+        public static final String GESCHENKFUR = "geschenkFur";
+        public static final String FURWEN = "furWen";
+
+    }
+}
--- src/test/java/sadilek/tests/ProductFilterTest.java
+++ src/test/java/sadilek/tests/ProductFilterTest.java
@@ -15,6 +15,7 @@ import sadilek.testcomponents.BaseTest;
 import sadilek.testcomponents.ExcelDataProvider;
 import sadilek.testcomponents.Retry;
 import sadilek.exceptions.ElementInteractionException;
+import sadilek.helpers.Constants.TestData;
 
 /**
  * Test class for validating the functionality of product filters.
@@ -24,12 +25,6 @@ import sadilek.exceptions.ElementInteractionException;
  * It uses TestNG for test case management and assertion.
  */
 public class ProductFilterTest extends BaseTest {
-    static final String HIGHLIGHT = "highlight";
-    static final String PRODUKTART = "produktart";
-    static final String MARKE = "marke";
-    static final String GESCHENKFUR = "geschenkFur";
-    static final String FURWEN = "furWen";
-
     /**
      * Test the product filters return revelant search listings
      * 
@@ -45,97 +40,85 @@ public class ProductFilterTest extends BaseTest {
      *                InterruptedException by Assert failing the test.
      */
     @Test(dataProvider = "getData", retryAnalyzer = Retry.class)
-    public void testProductFilters(HashMap<String, String> map) {
-        try {
-            /* local variables populated from our Excel data provider */
-            String highlight = map.get(HIGHLIGHT);
-            String produktart = map.get(PRODUKTART);
-            String marke = map.get(MARKE);
-            String geschenkFur = map.get(GESCHENKFUR);
-            String furWen = map.get(FURWEN);
+    public void testProductFilters(HashMap<String, String> facetNames) {
 
-            /* set the timeouts to be used by the POM classes */
-            int timeoutInSeconds = Integer.parseInt(getProperty("timeout"));
-            int retries = Integer.parseInt(getProperty("retries"));
+        /* local variables populated from our Excel data provider */
+        String highlight = facetNames.get(TestData.HIGHLIGHT);
+        String produktart = facetNames.get(TestData.PRODUKTART);
+        String marke = facetNames.get(TestData.MARKE);
+        String geschenkFur = facetNames.get(TestData.GESCHENKFUR);
+        String furWen = facetNames.get(TestData.FURWEN);
 
-            /* init the Landing Page and handle the modal popup */
-            LandingPage landingPage = new LandingPage(getDriver(), timeoutInSeconds, retries);
-            landingPage.gotoPage(getProperty("url"));
-            landingPage.handleModalPopup();
+        /* set the timeouts to be used by the POM classes */
+        int timeoutInSeconds = Integer.parseInt(getProperty("timeout"));
+        int retries = Integer.parseInt(getProperty("retries"));
 
-            ParfumPage parfumPage = landingPage.gotoParfumPage();
+        /* init the Landing Page and handle the modal popup */
+        LandingPage landingPage = new LandingPage(getDriver(), timeoutInSeconds, retries);
+        landingPage.gotoPage(getProperty("url"));
+        landingPage.handleModalPopup();
 
-            /* handle setting the filter for each search criteria */
-            parfumPage.setFilterProdukart(produktart);
-            parfumPage.setFilterMarke(marke);
-            parfumPage.setFilterHighlight(highlight);
-            parfumPage.setFilterGeschenkFur(geschenkFur);
-            parfumPage.setFilterFurWen(furWen);
+        ParfumPage parfumPage = landingPage.gotoParfumPage();
 
-            parfumPage.assertFacetsSetCorrectly(produktart, highlight, marke, geschenkFur, furWen);
+        /* handle setting the filter for each search criteria */
+        parfumPage.setFilterProdukart(produktart);
+        parfumPage.setFilterMarke(marke);
+        parfumPage.setFilterHighlight(highlight);
+        parfumPage.setFilterGeschenkFur(geschenkFur);
+        parfumPage.setFilterFurWen(furWen);
 
-            /* we refresh the page to ensure the right number of products are displayed */
-            getDriver().navigate().refresh();
-            // Thread.sleep(2000);
+        parfumPage.assertFacetsSetCorrectly(produktart, highlight, marke, geschenkFur, furWen);
+
+        /* we refresh the page to ensure the right number of products are displayed */
+        getDriver().navigate().refresh();
+
+        /*
+         * loop over the products on the current page so that we can assert the
+         * each one matches the facet/filter criteria. To limit duration of each test,
+         * only the products on the first result page are traversed. Pagintation support
+         * for multiple result page looping was avoided as a strategy since most
+         * combinations result in a single page of search results.
+         */
+        for (WebElement product : parfumPage.getProducts()) {
+            /* get the product page link from the WebElement */
+            String productPageUrl = parfumPage.getProductLink(product, retries);
 
             /*
-             * loop over the products on the current page so that we can assert the
-             * each one matches the facet/filter criteria. To limit duration of each test,
-             * only the products on the first result page are traversed. Pagintation support
-             * for multiple result page looping was avoided as a strategy since most
-             * combinations result in a single page of search results.
+             * out of stock products do not display highlights. No specific test case
+             * requirement given to account for out of stock items, so skip out of stock
+             * products when encountered
              */
-            for (WebElement product : parfumPage.getProducts()) {
-                /* get the product page link from the WebElement */
-                String productPageUrl = parfumPage.getProductLink(product, retries);
-
-                /*
-                 * out of stock products do not display highlights. No specific test case
-                 * requirement given to account for out of stock items, so skip out of stock
-                 * products when encountered
-                 */
-                if (parfumPage.isOutOfStock(product)) {
-                    log.info(productPageUrl + " product out of stock, skipping");
-                    continue;
-                }
-
-                /*
-                 * get the product page document via page source to speed up the execution since
-                 * we do not need to test the product page UI
-                 */
-                Document document = parfumPage.getDocument(product, productPageUrl);
-
-                /*
-                 * make sure each product is in view of the browser's viewport to avoid
-                 * before moving to it to avoid out of bounds issues
-                 */
-                parfumPage.scrollIntoView(product);
-                parfumPage.waitForVisibilityOf(product);
-                parfumPage.actionMoveToElement(product);
-
-                /*
-                 * asserts to test the relevance of the products listed. These methods have been
-                 * abstracted into the parfum page object class to keep the testProductFilters
-                 * test clean.
-                 */
-                parfumPage.assertEqualsProduktart(product, produktart, productPageUrl);
-                parfumPage.assertEqualsMarke(product, marke, productPageUrl);
-                parfumPage.assertEqualsHighlight(product, highlight, document, productPageUrl);
-                parfumPage.assertEqualsGeschenkFur(document, geschenkFur, productPageUrl);
-                // parfumPage.assertEqualsFurWen(document, furWen, productPageUrl);
+            if (parfumPage.isOutOfStock(product)) {
+                log.info(productPageUrl + " product out of stock, skipping");
+                continue;
             }
 
             /*
-             * catch any exceptions that might be thrown so that we can assert fail the test
-             * case
+             * get the product page document via page source to speed up the execution since
+             * we do not need to test the product page UI
              */
-        } catch (ElementInteractionException e) {
-            Assert.fail("Test failed due to an Element Interaction exception" + e.getMessage());
-        } catch (StaleElementReferenceException e) {
-            Assert.fail("Test failed due to a Stale element exception " + e.getMessage());
-        } catch (Exception e) {
-            Assert.fail("Test failed due to an unkown exception" + e.getMessage());
+            Document document = parfumPage.getDocument(product, productPageUrl);
+
+            /*
+             * make sure each product is in view of the browser's viewport to avoid
+             * before moving to it to avoid out of bounds issues
+             */
+            parfumPage.scrollIntoView(product);
+            parfumPage.waitForVisibilityOf(product);
+            parfumPage.actionMoveToElement(product);
+
+            /*
+             * asserts to test the relevance of the products listed. These methods have been
+             * abstracted into the parfum page object class to keep the testProductFilters
+             * test clean.
+             */
+            parfumPage.assertEqualsProduktart(product, produktart, productPageUrl);
+            parfumPage.assertEqualsMarke(product, marke, productPageUrl);
+            parfumPage.assertEqualsHighlight(product, highlight, document, productPageUrl);
+            parfumPage.assertEqualsGeschenkFur(document, geschenkFur, productPageUrl);
+            // parfumPage.assertEqualsFurWen(document, furWen, productPageUrl);
         }
+
     }
 
     /**
